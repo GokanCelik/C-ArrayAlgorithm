923e6c07182ef4b16123b44e1ba9f33e90a6b6a6
GokanCelik <105687348+GokanCelik@users.noreply.github.com>
2022-12-13 01:04:52 +0300

    .
    .

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            ArrayList a = new ArrayList() { "A", "B", "C", "D", "E", "F", "G", "H" };
+            ArrayList b = new ArrayList() { "1", "2", "3", "4", "5", "6", "7", "8" };
+            int sayac = 0;
+            etiket:
+            try
+            {
+                sayac++;
+                switch (sayac)
+                {
+                    case 1: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) + 2].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) + 1]).ToString(); break;
+                    case 2: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) + 2].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) - 1]).ToString(); break;
+                    case 3: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) - 2].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) + 1]).ToString(); break;
+                    case 4: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) - 2].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) - 1]).ToString(); break;
+                    case 5: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) - 1].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) + 2]).ToString(); break;
+                    case 6: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) + 1].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) + 2]).ToString(); break;
+                    case 7: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) + 1].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) - 2]).ToString(); break;
+                    case 8: listBox1.Items.Add(a[a.IndexOf(textBox1.Text.Substring(0, 1)) - 1].ToString() + b[b.IndexOf(textBox1.Text.Substring(1)) - 2]).ToString(); break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                goto etiket;
+            }
+            if (sayac != 9)
+                goto etiket;
+          
+          
+          
+       
+           
+            
+            
+           
+            
+           
+           
+            
+        }
+
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayac = 1, sayac2 = 0,a=0,l=-1,t=0,enbuyuk=0;
+            List<int> dizi = new List<int>() { 15, 2, 5, 2, 16, 5, 7, 8,9,10,11,15,13,14,15,16,16 };
+            List<int> tek_tekrar = new List<int>();
+            int[,] dizi2 = new int[20,2];
+            for (int i = 0; i < dizi.Count; i++)
+            {
+                l++;
+                t++;
+                tek_tekrar.Add(dizi[i]);
+                for (int j = 0; j < dizi.Count; j++)
+                {
+                    if (i!=j)
+                    {
+                        if (dizi[i] == dizi[j])
+                        {
+                            dizi.RemoveAt(j);
+                            j--;
+                            a = dizi[i];
+                            sayac++; 
+                        }
+                    }
+
+                    if (j==dizi.Count-1)
+                    {
+                        for (int k=l ; k < t; k++)
+                        {
+                            for (int  p= 0; p < 2; p++)
+                            {
+                                sayac2++;
+                                if (sayac2%2==0)
+                                    dizi2[k, p] = a; 
+                                else
+                                    dizi2[k, p] = sayac; 
+                            }
+                        }
+                        if (sayac>=enbuyuk)
+                            enbuyuk = sayac;
+                    }
+                }
+                sayac = 1;
+            }
+            if (enbuyuk==1)
+                tek_tekrar.ForEach(yazdir => listBox1.Items.Add(yazdir + " 'Sayısı dizide 1 kez geçmektedir"));
+            else
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if (enbuyuk == dizi2[i, j])
+                                listBox1.Items.Add(dizi2[i, j + 1] + " 'Sayısı dizide " + dizi2[i, j] + " kez geçmektedir");
+                        }
+                    }
+                }
+            }
+          
+        }
+    }
+}
